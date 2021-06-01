ebb9d4a8561ad19f209e75d35072c057ecf83546
Avisa-SEI <aap571420@gmail.com>
2021-06-01 15:17:43 -0700

    Greeting express application created

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-
+// require package/ dependencies
 const express = require('express');
 
+// initilize the express application
 const app = express();
 
 const port = 3000;
 
+
+// define our routes
+// routers run code as reponse to a request
 app.get('/greeting/:name', (req, res) => {
     res.send('Wow! Hello there, ' + req.params.name)
 })
