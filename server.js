e9843030395346b80abb8842215804b9be2a1cfd
Avisa-SEI <aap571420@gmail.com>
2021-06-01 16:08:54 -0700

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 
 // initilize the express application
 const app = express();
-
+ // constant
 const port = 3000;
 
+const magicBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+//  cached elements references
+let randomAnswers = magicBall[Math.floor(Math.random() * magicBall.length)]
 
 // define our routes
 // routers run code as reponse to a request
@@ -15,6 +18,10 @@ app.get('/greeting/:name', (req, res) => {
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
      res.send('Your tip is: ' + parseInt(req.params.tipPercentage))
+})
+
+app.get('/magic/:id', (req, res) => {
+    res.send(req.params.id + '?' + ' Answer: ' + randomAnswers)
 })
 
 app.listen(port, () => {
