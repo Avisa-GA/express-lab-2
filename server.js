bd26156bde0dae0cb8c3ee0d81a0d0ecb7af8f51
Avisa-SEI <aap571420@gmail.com>
2021-06-01 15:13:20 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/greeting/:name', (req, res) => {
+    res.send('Wow! Hello there, ' + req.params.name)
+})
+
+app.listen(port, () => {
+    console.log('Express is listening on port', port)
+})
